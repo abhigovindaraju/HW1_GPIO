d3bd2c1aa7b6a6c87c653587073e031926954d85
abhigovindaraju <abhigovindaraju@gmail.com>
2015-04-11 20:15:19 -0700

    Create HW1_GPIO.c

---
+++ HW1_GPIO.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define PINSEL0 0x4002C000
+#define FIO0DIR 0x2009C000
+#define FIO0SET 0x2009C018
+#define FIO0CLR 0x2009C01C
+
+
+int main(void)
+{
+  unsigned int volatile * setGPIO = (unsigned int *)PINSEL0;
+  unsigned int volatile * setDIR = (unsigned int *)FIO0DIR;
+  unsigned int volatile * setHIGH = (unsigned int *)FIO0SET;
+  unsigned int volatile * setLOW = (unsigned int *)FIO0CLR;
+
+  *setGPIO = *setGPIO & 0xcFFFFFFF;
+  *setDIR = (*setDIR & 0x0 ) | 0x00008000;
+  *setHIGH = (*setHIGH & 0x0 ) | 0x00008000;
+  *setLOW = (*setLOW & 0x0 ) | 0x00008000;
+
+  return 0;
+}
